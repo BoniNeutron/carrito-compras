6662b8a24707969ae0f11646738b02a9079a1056
boni <x.bonillaa@gmail.com>
2020-11-02 22:49:28 -0500

    ver cambios en git

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// variables
+
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+
+
+cargarEventListeners();
+function cargarEventListeners() {
+
+}
